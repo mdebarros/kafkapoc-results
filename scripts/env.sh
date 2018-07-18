4aecc160934835ce04c58804935e818ff5be830a
Miguel de Barros <miguel@debarros.me>
2018-07-18 11:52:57 +0200

    added default env.sh

---
+++ scripts/env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export PERF_ENV_01_USER=user
+export PERF_ENV_01_IP=127.0.0.1
+export PERF_ENV_01_ID=~/.ssh/key.pem
+export PERF_JMETER_USER=user
+export PERF_JMETER_IP=127.0.0.1
+export PERF_JMETER_ID=~/.ssh/key.pem
