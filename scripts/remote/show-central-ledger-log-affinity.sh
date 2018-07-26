c6448bc49a22cad3b93a61cfdad4126c9a877f56
Miguel de Barros <miguel@debarros.me>
2018-07-26 15:17:48 +0200

    fixing logs again

--- scripts/remote/show-central-ledger-log-affinity.sh
+++ scripts/remote/show-central-ledger-log-affinity.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tail -F ./central-ledger/console-central-ledger-prepare.log ./central-ledger/console-central-ledger-position.log ./central-ledger/console-central-ledger-transfer.log ./central-ledger/console-central-ledger-fulfil.log
+tail -F ./central-ledger/console-central-ledger-prepare-${FSP1}.log ./central-ledger/console-central-ledger-prepare-${FSP2}.log ./central-ledger/console-central-ledger-position-${FSP1}.log ./central-ledger/console-central-ledger-position-${FSP2}.log ./central-ledger/console-central-ledger-transfer.log ./central-ledger/console-central-ledger-fulfil.log
 
