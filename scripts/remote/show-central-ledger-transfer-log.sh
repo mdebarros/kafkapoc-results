d5d249d2fe6b5e327a78e1a7fba6e20566811be3
Miguel de Barros <miguel@debarros.me>
2018-07-26 15:08:54 +0200

    fixing log script issues

--- scripts/remote/show-central-ledger-transfer-log.sh
+++ scripts/remote/show-central-ledger-transfer-log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tail -100f ./central-ledger/console-central-ledger-prepare.log ./central-ledger/console-central-ledger-position.log ./central-ledger/console-central-ledger-transfer.log ./central-ledger/console-central-ledger-fulfil.log
+tail -100f ./central-ledger/console-central-ledger-transfer.log
 
